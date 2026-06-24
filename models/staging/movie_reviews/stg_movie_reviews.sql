b04881ab1316c2feb92a69d9d15d1c3584c6459e
Toshi <34364637+toshihue@users.noreply.github.com>
2026-06-24 09:37:00 +0900

    Update stg_movie_reviews.sql
    dedupe

--- models/staging/movie_reviews/stg_movie_reviews.sql
+++ models/staging/movie_reviews/stg_movie_reviews.sql
@@ -1,8 +1,22 @@
-with 
-
-source as (
+with source as (
 
     select * from {{ source('movie_reviews', 'raw_movie_reviews') }}
+
+),
+
+ranked as (
+
+    select
+        id,
+        movie_id,
+        response_time,
+        review_text,
+        sentiment_value,
+        row_number() over (
+            partition by id
+            order by response_time desc, movie_id desc
+        ) as row_num
+    from source
 
 ),
 
@@ -13,12 +27,12 @@ renamed as (
         movie_id,
         response_time as review_time,
         review_text as review_txt,
-    case 
-        when sentiment_value = 0 then -1
-        else 1
-    end as actual_sentiment
-
-    from source
+        case
+            when sentiment_value = 0 then -1
+            else 1
+        end as actual_sentiment
+    from ranked
+    where row_num = 1
 
 )
 
